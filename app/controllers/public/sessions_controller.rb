eeaa9c71f038d893470f80aeb84633ae9f16c1aa
densuke5G <129590446+densuke5G@users.noreply.github.com>
2023-06-13 05:11:01 +0000

    add

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -20,6 +20,16 @@ class Public::SessionsController < Devise::SessionsController
 
   # protected
 
+  private
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
