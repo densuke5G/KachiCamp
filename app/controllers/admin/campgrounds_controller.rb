65c9ba9f06b0107cd284d6de81eb2bf61ea261e9
densuke5G <129590446+densuke5G@users.noreply.github.com>
2023-06-17 16:51:37 +0000

    tagcreate

--- app/controllers/admin/campgrounds_controller.rb
+++ app/controllers/admin/campgrounds_controller.rb
@@ -5,28 +5,51 @@ class Admin::CampgroundsController < ApplicationController
 
   def index
     @campgrounds = Campground.all
+    @tag_list = Tag.all
   end
 
   def show
     @campground = Campground.find(params[:id])
+    @campground_tags = @campground.tags
   end
 
   def edit
     @campground = Campground.find(params[:id])
+    @tag_list = @campground.tags.pluck(:tag_name).join(',')
   end
 
   def create
     campground = Campground.new(campground_params)
+    campground.is_confirmed = confirmed
 
-    campground.save
-    redirect_to admin_campgrounds_path
+    # タグを,で区切り配列にする
+    tag_list = params[:campground][:tag_name].split(',')
+
+    if campground.save
+      campground.save_tag(tag_list)
+      redirect_to admin_campgrounds_path, notice:'投稿完了しました'
+    else
+      render:new
+    end
   end
 
   def update
     campground = Campground.find(params[:id])
 
-    campground.update(campground_params)
-    redirect_to admin_campground_path(campground.id)
+    tag_list = params[:campground][:tag_name].split(',')
+
+    if campground.update(campground_params)
+      # campground.idに紐付いたtagを削除する
+      @old_relations = Camptag.where(campground_id: campground.id)
+      @old_relations.each do |relation|
+        relation.delete
+      end
+
+      campground.save_tag(tag_list)
+      redirect_to admin_campground_path(campground.id), notice:'編集完了しました'
+    else
+      render:edit
+    end
   end
 
   def destroy
